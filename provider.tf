7968ecf378bfcbc8cceddacfe58a7ad1e87e0129
RUSTAT Jonathan <jonathan.rustat@soprasteria.com>
2025-01-17 09:02:17 +0100

    change region

--- provider.tf
+++ provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "5.83.1"
     }
   }
+
+  backend "s3" {
+    bucket         = "jrt-terraform-state-bucket-eu-west-1"
+    key            = "terraform/state"
+    region         = "eu-west-1"
+    dynamodb_table = "jrt-terraform-lock-table-eu-west-1"
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
