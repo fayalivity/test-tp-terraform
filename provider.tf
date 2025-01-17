c84409761343318774738d3cf4ed54cfc05c28b0
RUSTAT Jonathan <jonathan.rustat@soprasteria.com>
2025-01-17 14:28:42 +0100

    remove backend configuration from tf file

--- provider.tf
+++ provider.tf
@@ -7,10 +7,6 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "jrt-terraform-state-bucket-eu-west-1"
-    key            = "terraform/${var.env}/state"
-    region         = "eu-west-1"
-    dynamodb_table = "jrt-terraform-lock-table-eu-west-1-${var.env}"
   }
 }
 
